af8e09d2be092fb531b83fe249f954ea61e7994b
Emrehan <emremart1234@gmail.com>
2024-05-25 17:06:02 +0300

    bir şeyler deneme

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const SignUp= document.getElementById("sign-up");
+
+
+SignUp.addEventListener("click", event=> {
+    
+    window.open("Kayit Olma Sayfasi", "_blank","width=600", "height=500");
+});
